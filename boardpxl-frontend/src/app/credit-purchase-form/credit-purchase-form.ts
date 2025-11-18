13704a60f90763249a3b73023f182ed387053d42
TitouanGalles <tgalles@iutbayonne.univ-pau.fr>
2025-11-18 15:20:33 +0100

    création component pour le formulaire d'achat de crédits

---
+++ boardpxl-frontend/src/app/credit-purchase-form/credit-purchase-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-credit-purchase-form',
+  standalone: false,
+  templateUrl: './credit-purchase-form.html',
+  styleUrl: './credit-purchase-form.scss',
+})
+export class CreditPurchaseForm {
+
+}
