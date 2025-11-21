5374c93709bd1437297bf7ce7532f8291adc5203
LucaDeBrito <lucadb0603@gmail.com>
2025-11-21 08:53:15 +0100

    PS-61 added route

--- boardpxl-frontend/src/app/app-routing-module.ts
+++ boardpxl-frontend/src/app/app-routing-module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import {LoginPage} from './login-page/login-page';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login-page', component: LoginPage}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes), HttpClientModule],
+  exports: [RouterModule, HttpClientModule]
 })
 export class AppRoutingModule { }
