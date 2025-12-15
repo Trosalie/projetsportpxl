fac4a9d2de630f548ec2761aab08db10cf1ab658
Kevin <koulai001@univ-pau.fr>
2025-12-15 14:39:57 +0100

    PS-61 Modification front pour coller aux maquettes

--- boardpxl-frontend/src/app/header/header.ts
+++ boardpxl-frontend/src/app/header/header.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class Header {
   userName: string = 'Test User';
+
+  constructor(private router: Router) {}
+
+  isLoginPage(): boolean {
+    return this.router.url === '/login';
+  }
 }
